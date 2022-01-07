f1ab22d7096f2b7160af3c1f2f2dfbd6f76dab52
julihcho <juli.h.cho@gmail.com>
2022-01-07 12:38:30 -0500

    make fox yell

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,14 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+//define function for alert
+function sayOuch()
+{
+	alert("Ouch! Stop poking me!");
+}
+
+//use CSS selector to identify an element
+var foxImage = document.querySelector('img'); //select all images
+
+//assign function to onclick event on that element
+foxImage.onclick = sayOuch;
